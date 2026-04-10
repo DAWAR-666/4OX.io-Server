d6f7a4b2ead7d1750d9b280c428ad5beb5f654df
DAWAR <dawarpratham8@gmail.com>
2026-04-10 18:49:32 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app=express();
+
+app.use(cors({origin:'http://localhost:5173'}))
+
 app.use(express.json());
 
 app.get('/',(req,res)=>{
