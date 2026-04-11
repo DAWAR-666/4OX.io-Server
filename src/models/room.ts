6a29f7b761fbea2f6586776d4fc2d7011b055651
DAWAR <dawarpratham8@gmail.com>
2026-04-11 16:31:10 +0530

    created roomSchema

---
+++ src/models/room.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+interface IRoom extends mongoose.Document{
+    roomID:string;
+    players:mongoose.Types.ObjectId[];
+    gameState:"waiting" | "playing" | "finished";
+    winner?:mongoose.Types.ObjectId | null;
+}
+const roomSchema=new mongoose.Schema<IRoom>({
+    roomID:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        minLength:6,
+        maxLength:6,
+        uppercase:true
+    },
+    players:{
+        type:[{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }],
+        required:true,
+        validate(value: mongoose.Types.ObjectId[]) {
+            if (value.length < 1 || value.length > 2) {
+                throw new Error('A room must have 1 or 2 players');
+            }
+        }
+    },
+    gameState:{
+        type:String,
+        enum:["waiting","playing","finished"],
+        default:"waiting",
+    },
+    winner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        default:null
+    }
+},{timestamps:true})
+const Room=mongoose.model("Room",roomSchema);
+export default Room;
