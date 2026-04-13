5af20eb34c62f5b1e41e434a7b8ca043d29d5f22
DAWAR <dawarpratham8@gmail.com>
2026-04-13 19:07:51 +0530

    created roomJoin socket event handler

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,5 +1,8 @@
 import { Server } from "socket.io";
 import http from "http";
+import Room from "../models/room";
+import { getGame ,createGame,setGame,deleteGame} from "./game";
+import { Player } from "./types";
 export const initializeSocket=(server:http.Server)=>{
     const io=new Server(server,{
         cors:{
@@ -8,9 +11,45 @@ export const initializeSocket=(server:http.Server)=>{
         }
     });
     io.on("connection",(socket)=>{
-        console.log("a user connected");
+        console.log(socket.id+" connected");
+        
+        socket.on("joinRoom",async(roomId:string)=>{
+            const isRoom=await Room.findOne({roomId});
+            if(!isRoom){
+                console.log("Room not found: "+roomId);
+                socket.emit("error","Room not found");
+                return;
+            }
+            socket.join(roomId);
+            console.log(socket.id+" joined room "+roomId);
+            let gameState=getGame(roomId);
+            if(!gameState){
+                const newGame=createGame();
+                const player:Player={id:null,socketId:socket.id,userName:"player1",symbol:'X',queue:[]};
+                newGame.players.push(player);
+                setGame(roomId,newGame);
+                gameState=newGame;
+                socket.emit("gameState",gameState);
+            }
+            else if(gameState?.players.length===1){
+                const player:Player={id:null,socketId:socket.id,userName:"player2",symbol:'O',queue:[]};
+                gameState?.players.push(player);
+                setGame(roomId,gameState);
+                gameState.status="playing";
+                gameState.currentTurn=gameState.players[0].socketId;
+                setGame(roomId,gameState);
+                io.to(roomId).emit("gameState",gameState);
+            }
+            else{
+                socket.emit("error","Room is full");
+                socket.leave(roomId);
+                return;
+            }
+
+        })
+        
         socket.on("disconnect",()=>{
-            console.log("user disconnected");
+            console.log(socket.id+" disconnected");
         }
         );
     });
