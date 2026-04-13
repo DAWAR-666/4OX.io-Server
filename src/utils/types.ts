07160ed4405f30f50525b4e0951de49330f149d5
DAWAR <dawarpratham8@gmail.com>
2026-04-13 18:02:03 +0530

    defining game types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,3 +4,25 @@ import { IUser } from '../models/user'
 export interface AuthRequest extends Request {
   user?: IUser
 }
+export type piece={
+    id:string,
+    cellIndex:number
+}
+export type Player={
+    id:string|null,
+    socketId:string,
+    userName:string|null,
+    symbol:"X"|"O",
+    queue:piece[]
+}
+export type Game={
+    board:(string|null)[],
+    players:Player[],
+    currentTurn:string,
+    status:"waiting"|"playing"|"finished",
+    winner:string|null,
+    dissappearingPcs:string|null
+}
+export type GameStore = {
+  [roomId: string]: Game
+}
